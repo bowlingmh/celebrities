b766aef1408d5deb872cdd93ea13ac2e93758553
Michael Bowling <mbowling@ualberta.ca>
2020-12-05 18:51:55 -0700

    Add reset method for Room; basic endpoints

--- app.py
+++ app.py
@@ -25,7 +25,19 @@ class Room:
                      f"{self.room_id}:names_guessed",
                      name)
 
+  def reset(self):
+    redis_conn.sunionstore(f"{self.room_id}:names_left",
+                           f"{self.room_id}:names_left",
+                           f"{self.room_id}:names_guessed")
+    redis_conn.sdiffstore(f"{self.room_id}:names_guessed",
+                          f"{self.room_id}:names_guessed",
+                          f"{self.room_id}:names_left")
 
-    
-    
 
+@app.route('/<room_id>/words', methods=['GET', 'POST'])
+def words(room_id):
+    pass
+
+@app.route('/<room_id>/play')
+def play(room_id):
+    pass
